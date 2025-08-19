38d1e980d122f6b6e36192d3aa3402b5c445e089
José ismael correia <62517288+Ismael1995Developer@users.noreply.github.com>
2025-08-19 17:34:43 -0300

    inclusao de funções javaScript

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,43 @@ import Alert from 'bootstrap/js/dist/alert';
 
 // or, specify which plugins you need:
 import { Tooltip, Toast, Popover } from 'bootstrap';
+
+// importação do utilitario
+import { calcular } from './ultitario';
+
+// declaração de variavel para document.ID
+const inputSaidaCal = document.querySelector("#inputSaidaCal");
+const btnResul = document.querySelector("#btnResul");
+const btnC = document.querySelector("#btnC");
+const botoes = document.querySelectorAll("button");
+
+let display = "";
+
+// função apagar 
+btnC.addEventListener("click", () => {
+    display = "";              
+    inputSaidaCal.value = display; 
+});
+
+
+
+// função de captura de eventos e validação de number
+botoes.forEach(btn => btn.addEventListener("click", () => {
+    display += btn.value;
+    inputSaidaCal.value = display;
+}));
+
+// função resultado 
+btnResul.addEventListener("click", () => {
+    let partes = display.match(/\d+|\+|\-|\√|\^|\*|\/|%|\./g);
+
+    let numeros = partes.map(parte => 
+        ["+", "-", "*", "/", "^", "%","√"].includes(parte) ? parte : Number(parte)
+    );
+
+    let resultado = calcular(numeros);
+    inputSaidaCal.value = resultado;
+    display = resultado.toString();
+});
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 
 export default {
+  base: '/Projeto-Calculadora-JS/', // Nome do repositorio que esta o projeto no git
   root: resolve(__dirname, 'src'),
   build: {
     outDir: '../dist'
