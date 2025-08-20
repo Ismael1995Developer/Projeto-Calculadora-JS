bfb533551dbf2818c0ad44923867b7d53251deb0
José ismael correia <62517288+Ismael1995Developer@users.noreply.github.com>
2025-08-20 12:03:16 -0300

    ajustes de responsividade e melhorias javascript

--- src/js/main.js
+++ src/js/main.js
@@ -9,42 +9,96 @@ import Alert from 'bootstrap/js/dist/alert';
 // or, specify which plugins you need:
 import { Tooltip, Toast, Popover } from 'bootstrap';
 
-// importação do utilitario
-import { calcular } from './ultitario';
 
 // declaração de variavel para document.ID
 const inputSaidaCal = document.querySelector("#inputSaidaCal");
 const btnResul = document.querySelector("#btnResul");
 const btnC = document.querySelector("#btnC");
 const botoes = document.querySelectorAll("button");
+const btnCe = document.querySelector("#btnCe");
 
 let display = "";
 
 // função apagar 
-btnC.addEventListener("click", () => {
-    display = "";              
+btnCe.addEventListener("click", () => {
+    display = "";
     inputSaidaCal.value = display; 
 });
 
-
+// função excluir caracter 
+btnC.addEventListener("click", () => {
+    display = display.slice(0, -1);
+    inputSaidaCal.value = display; 
+})
 
 // função de captura de eventos e validação de number
 botoes.forEach(btn => btn.addEventListener("click", () => {
+    // pega o ultimo numero digitado 
+    let ultimaParte = display.split(/[\+\-\*\/\^%√]/).pop();
+
+    // Validação de ponto 
+    if (btn.value === "." && ultimaParte.includes(".")) {
+        return;
+    }
+
     display += btn.value;
     inputSaidaCal.value = display;
 }));
 
-// função resultado 
-btnResul.addEventListener("click", () => {
-    let partes = display.match(/\d+|\+|\-|\√|\^|\*|\/|%|\./g);
+const calcular = (partes) => {
+    let resultado = partes[0]; //começa com o primeiro numero armazenado 
 
+    
+    for (let i = 0; i < partes.length; i++) {
+        let operador = partes[i];
+
+         // condição se for raiz mudar a ordem de operação 
+         if (operador === "√") {
+            let numero = parseFloat(partes[i + 1]);
+            resultado = Math.sqrt(numero);
+
+        } else if (!isNaN(operador)) {
+        // primeiro número
+            if (resultado === undefined) { 
+                resultado = parseFloat(operador); 
+            }
+        } else {
+            // operadores normais
+            let numero = parseFloat(partes[i + 1]);
+
+        switch (operador) {
+            case "+": resultado += numero; break;
+            case "-": resultado -= numero; break;
+            case "*": resultado *= numero; break;
+            case "/": resultado /= numero; break;
+            case "^": resultado **= numero; break;
+            case "%": resultado = (resultado * numero) / 100; break;
+    }
+    i++;
+    }
+  }
+  return resultado;
+}
+
+try {
+    // função resultado 
+btnResul.addEventListener("click", () => {
+    let partes = display.match(/(\d+(\.\d+)?)|[\+\-\*\/√\^%]/g);
+    
     let numeros = partes.map(parte => 
         ["+", "-", "*", "/", "^", "%","√"].includes(parte) ? parte : Number(parte)
     );
 
     let resultado = calcular(numeros);
-    inputSaidaCal.value = resultado;
-    display = resultado.toString();
-});
+        if (isNaN(resultado)) {
+            inputSaidaCal.value = "Operação invalida";
+        }else {
+            inputSaidaCal.value = resultado;
+            display = "";
+        }
+   });
+} catch (error) {
+    inputSaidaCal = "erro na função";
+}
 
 
